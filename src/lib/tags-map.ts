18954aab507d9a36cd5128b37daa74a971b58ea5
Touha Zohair <touha98@gmail.com>
2023-12-09 00:23:29 +0600

    projects and icons

--- src/lib/tags-map.ts
+++ src/lib/tags-map.ts
@@ -1,4 +1,8 @@
 export const tagsMap: Record<string, string> = {
   nextjs: "/images/tags/nextjs.svg",
   tailwindcss: "/images/tags/tailwindcss.svg",
+  express: "/images/tags/express.png",
+  "socket.io": "/images/tags/socket-io.svg",
+  "web components": "/images/tags/web-components.svg",
+  prisma: "/images/tags/prisma.jpg",
 };
