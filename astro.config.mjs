d1592bdfd247149e74292d58a7a8a4cd97222791
Touha Zohair <touha98@gmail.com>
2023-12-09 20:46:19 +0600

    astro sitemap added, sharp installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), mdx()]
+  site: "https://touha.dev",
+  integrations: [tailwind(), preact(), mdx(), sitemap()],
 });
