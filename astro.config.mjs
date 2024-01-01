93d96cc03dfbc609a47c4b1e7697a5ed2c098317
Touha Zohair <touha98@gmail.com>
2024-01-01 21:44:18 +0600

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://touha.dev",
+  site: "https://www.touha.dev",
+
   integrations: [tailwind(), preact(), mdx(), sitemap()],
 });
